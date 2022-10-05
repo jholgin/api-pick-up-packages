376a7430e3dca5bb6e3919c8229cf271c0df9959
gcuriel02 <gcuriel@uninorte.edu.co>
2022-10-04 21:49:33 -0500

    tratando de entender :c

--- src/controller/getOrders.js
+++ src/controller/getOrders.js
@@ -6,3 +6,24 @@ export const getOrders =  (req, res) => {
     prueba(getConnecion());
   }
 
+//ejemplo con fetch
+
+//   fetch("api/recor", {
+//   method: "POST",
+//   body: bodyContent,
+//   headers: headersList
+// }).then(res => {
+//   return res.json() 
+// }).then(data => {
+//   let rec
+//   if (data.fecha=="0" && data.hora=="0"){
+//       document.getElementById("rec").innerHTML ='<li> No hay datos en esta posición </li>'
+//       }else{
+//           data = data.positions
+//            rec = data.map(function(bar){
+//           fec=bar.Fecha.split("T")
+//           return '<li>'+fec[0]+' '+bar.Hora+'</li>'
+//         })
+//         document.getElementById("rec").innerHTML = rec;}
+        
+// })
--- src/routes/order.js
+++ src/routes/order.js
@@ -8,6 +8,16 @@ const router = Router();
 // Obtener todas las ordenes
 router.get("/orders/",getOrders);
 
+//ejemplo que tenemos en proyecto diseño
+// app.get('/getData', (req, res) => {
+//   res.status(200).json({
+//       lat: lat,
+//       long: long,
+//       date: date,
+//       time: time
+//   })
+// })
+
 router.get("/orders/create", (req, res) => {
   res.send("Ordenes create");
   prueba(getConnecion());
